56162569d579b295f35da35b22f603211b3bf385
sksmatt <matiasvarone@gmail.com>
2012-08-15 17:50:21 -0300

    removing viewport meta-tag

--- header.php
+++ header.php
@@ -18,7 +18,6 @@
 <meta name="viewport" content="width=device-width" />
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <title><?php wp_title(); ?></title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     
     <link rel="profile" href="http://gmpg.org/xfn/11" />
     <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />

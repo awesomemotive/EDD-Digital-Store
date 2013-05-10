0c960c643545f79421e3e93ecafbb52781b6cd58
Pippin Williamson <pippin@pippinsplugins.com>
2013-05-10 15:52:59 -0500

    Updated version number to 1.2.1

--- functions.php
+++ functions.php
@@ -20,7 +20,7 @@
 
 define( 'EDD_DIGITAL_STORE_STORE_URL', 'https://easydigitaldownloads.com' );
 define( 'EDD_DIGITAL_STORE_THEME_NAME', 'Digital Store Theme' );
-define( 'EDD_DIGITAL_STORE_VERSION', '1.2' );
+define( 'EDD_DIGITAL_STORE_VERSION', '1.2.1' );
 
 
 // Set content width

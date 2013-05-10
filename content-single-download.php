61a802546ec72563f65dfb08fc7598ccac74f4a1
Pippin Williamson <pippin@pippinsplugins.com>
2013-05-10 15:46:55 -0500

    Whitespace

--- content-single-download.php
+++ content-single-download.php
@@ -13,9 +13,9 @@
 do_action( 'digitalstore_before_content', $post ); ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-        
+
         <div class="entry">
-            
+
             <div class="entry-image">
                 <?php if ( has_post_thumbnail() ): ?>
                     <?php the_post_thumbnail( 'digitalstore_thumb_321x292' ); ?>
@@ -23,35 +23,35 @@ do_action( 'digitalstore_before_content', $post ); ?>
                     <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/nopic-large.gif" alt="<?php the_title_attribute(); ?>"/>
                 <?php endif ?>
             </div><!-- entry-image -->
-            
+
             <header class="entry-header">
-            
+
                 <h1 class="entry-title">
                     <?php the_title(); ?>
                 </h1><!-- entry-title -->
-                
+
                 <div class="entry-price">
                     <div class="price"><?php echo digitalstore_edd_the_price( $post->ID ); ?></div>
                     <?php do_action( 'digitalstore_after_price', $post ); ?>
                 </div><!-- entry-price -->
-                
+
                 <?php do_action( 'digitalstore_add_to_cart', $post ); ?>
-                
+
                 <div class="entry-summary">
                     <?php the_excerpt(); ?>
                 </div><!-- .entry-summary -->
-            
+
             </header><!-- .entry-header -->
-            
+
             <h3 class="section-title">
                 <?php _e( 'Description', 'edd-digitalstore' ); ?>
             </h3><!-- section-title -->
-            
+
             <div class="entry-content">
                 <?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'edd-digitalstore' ) ); ?>
                 <?php wp_link_pages( array( 'before' => '<div class="page-link">' . __( 'Pages:', 'edd-digitalstore' ), 'after' => '</div>' ) ); ?>
             </div><!-- .entry-content -->
-            
+
             <footer class="entry-utility">
                 <?php
                     $tag_list = get_the_term_list( $post->ID, 'download_tag', '', ', ', '' );
@@ -67,9 +67,9 @@ do_action( 'digitalstore_before_content', $post ); ?>
                      );
                 ?>
             </footer><!-- .entry-meta -->
-            
+
         </div><!-- .entry -->
-        
+
         <?php get_template_part( 'entry-meta' ); ?>
 
 </article><!-- #post-<?php the_ID(); ?> -->
--- single.php
+++ single.php
@@ -15,23 +15,23 @@ $post_type = get_post_type();
 get_header( $post_type ); ?>
 
     <section id="main" role="main">
-        
+
         <?php do_action( 'digitalstore_before_template_header' ); ?>
-        
+
         <?php while ( have_posts() ) : the_post(); ?>
-            
+
             <?php if ( $post_type == 'post' ): ?>
                 <?php get_template_part( 'content-single', get_post_format() ); ?>
             <?php else: ?>
                 <?php get_template_part( 'content-single', $post_type ); ?>
             <?php endif ?>
-            
+
             <?php if ( comments_open() ): ?>
                 <?php comments_template( '', true ); ?>
             <?php endif ?>
-            
+
         <?php endwhile; ?>
-        
+
     </section><!-- #main -->
 
 <?php get_sidebar( $post_type ); ?>

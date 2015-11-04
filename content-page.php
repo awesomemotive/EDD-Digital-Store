76b618f7a4faffdcd78aafc97aa7cf6b732b59f0
Sean Davis <sdavis2702@gmail.com>
2015-11-03 21:21:06 -0800

    remove excerpt from page template #43

--- content-page.php
+++ content-page.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Partial: Content Page 
+ * Partial: Content Page
  *
  * @package      Digital Store
  * @author       Easy Digital Downloads - http://easydigitaldownloads.com
@@ -17,28 +17,24 @@ do_action( 'digitalstore_before_content', $post ); ?>
     <div class="entry">
 
         <header class="entry-header">
-            
+
             <h1 class="page-title"><?php the_title(); ?></h1>
-            
-            <?php if ( has_excerpt() ): ?>
-                <div class="intro-meta"><?php the_excerpt() ?></div>
-            <?php endif ?>
-            
+
         </header><!-- .entry-header -->
-        
+
         <?php if ( has_post_thumbnail() ): ?>
             <div class="entry-image">
                 <?php the_post_thumbnail(); ?>
             </div>
         <?php endif ?>
-        
+
         <div class="entry-content">
             <?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'edd-digitalstore' ) ); ?>
             <?php wp_link_pages( array( 'before' => '<div class="page-link">' . __( 'Pages:', 'edd-digitalstore' ), 'after' => '</div>' ) ); ?>
         </div><!-- .entry-content -->
-        
+
         <?php edit_post_link(); ?>
-        
+
     </div>
 
 </article><!-- #post-<?php the_ID(); ?> -->

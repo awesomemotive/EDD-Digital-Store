a886d6e73b389c7e881925d4f739add0f9af33b0
Sean Davis <sdavis2702@gmail.com>
2016-04-19 08:00:05 -0700

    adjust JS syntax after WordPress 4.5 update #61

--- js/scripts.dev.js
+++ js/scripts.dev.js
@@ -31,12 +31,12 @@ var digitalstore_theme_js_params;
         menu.toggle();
     });
     $('body').on('click.eddAddToCart', '.edd-add-to-cart',function (e) {
-        var drop = $('.dropdown-menu a[href=#addtocart]');
+        var drop = $('.dropdown-menu a[href="#addtocart"]');
         if ( drop.length > 0 && digitalstore_theme_js_params.checkout_uri.length > 0 ) {
                 drop.text(digitalstore_theme_js_params.in_gotocheckout).attr('href',digitalstore_theme_js_params.checkout_uri);
         }
     });
-    $('.dropdown-menu a[href=#addtocart]').on('click', function (e) {
+    $('.dropdown-menu a[href="#addtocart"]').on('click', function (e) {
         var self = $(this);
         if ( self.attr('href') === '#addtocart' ) {
             self.parents('.add-to-cart').find('input.edd-add-to-cart').click();
